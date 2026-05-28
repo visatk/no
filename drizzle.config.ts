8cb57273ddcaa288955812664458296dd95164cf
visatk <visatk29@gmail.com>
2026-05-29 01:18:21 +0600

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'sqlite',
-  schema: './src/db/schema.ts',
-  out: './drizzle',
+  schema: './worker/db/schema.ts',
+  out: './migrations',
 });
